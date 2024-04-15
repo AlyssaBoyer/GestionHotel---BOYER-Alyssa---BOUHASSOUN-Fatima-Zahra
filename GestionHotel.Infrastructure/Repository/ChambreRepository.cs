535a70b14fc9c327d19a0b8069e140d3d2e9cd02
Alyssa <a.boyer@groupe-aen.info>
2024-04-15 15:11:13 +0200

    continuer chambre reservation

---
+++ GestionHotel.Infrastructure/Repository/ChambreRepository.cs
@@ -0,0 +1,10 @@
+using GestionHotel.Apis.Models.Entities;
+using System.Collections.Generic;
+
+namespace GestionHotel.Infrastructure.Repository
+{
+    public class ChambreRepository : IChambreRepository
+    {
+        // Implémentation de l'accès aux données des chambres
+    }
+}

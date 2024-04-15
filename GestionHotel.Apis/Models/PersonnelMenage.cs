1978d9c1ef3e074fd4aa73a3b32ff8c5caa11241
Alyssa <a.boyer@groupe-aen.info>
2024-04-15 12:19:56 +0200

    Update PersonnelMenage.cs

---
+++ GestionHotel.Apis/Models/PersonnelMenage.cs
@@ -0,0 +1,10 @@
+namespace GestionHotel.Models
+{
+    public class PersonnelMenage
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Fonction { get; set; }
+    }
+}

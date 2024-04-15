b051e901e912623cfc55cc2ae09715d88346dd44
Alyssa <a.boyer@groupe-aen.info>
2024-04-15 15:22:15 +0200

    commit

--- GestionHotel.Infrastructure/Repository/ChambreRepository.cs
+++ GestionHotel.Infrastructure/Repository/ChambreRepository.cs
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-using GestionHotel.Apis.Models.Entities;
-using System.Collections.Generic;
 
-namespace GestionHotel.Infrastructure.Repository
-{
-    public class ChambreRepository : IChambreRepository
-    {
-        // Implémentation de l'accès aux données des chambres
-    }
-}
-=======
 using GestionHotel.Apis.Models;
 using System;
 using System.Collections.Generic;
@@ -27,4 +16,3 @@ namespace GestionHotel.Apis.Services
         // Autres méthodes de manipulation des chambres peuvent être implémentées ici
     }
 }
->>>>>>> 25a5e2943d5e143efda83aa70911a352e8386239
